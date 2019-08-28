6dd2b2e9b473018d11e61f547005b9fd38a90e5b
Enziet <Simply+Enziet@users.noreply.github.com>
2019-08-28 15:35:36 -0700

    Create steamworkshop-download-userscript.js
    Initial commit to get version control up and going in vs code.

---
+++ steamworkshop-download-userscript.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name           Steam Workshop Downloader Button
+// @namespace      http://github.com/fgblomqvist
+// @description    Adds a button to the Steam Workshop pages (even collections) that lets you head straight to the specific addon page at steamworkshop.download
+// @include        *steamcommunity.com/sharedfiles/filedetails/?id=*
+// @version        1.0.2
+// @downloadURL    http://steamworkshopdownloader.com/static/files/swd.user.js
+// ==/UserScript==
+
+// Let's first check to see if this is a shared collection. If it is, iterate though all the collectionItem divs and add the link to
+// http://steamworkshop.download/download/view/id from the id parsed by the div's id: 'sharedfile_[id here]'
+
+var collectionItems = document.getElementsByClassName("collectionItem");
+
+if (collectionItems.length > 0) {
+}
+else {
+
+  var patt = new RegExp("[0-9]{2,15}");
+  var id = patt.exec(document.URL);
+
+  var realButton = document.getElementById("SubscribeItemBtn");
+
+  // shorten the text in the box because it will be in the way
+  realButton.parentNode.getElementsByTagName("h1")[0].innerHTML = "Download/Subscribe to the right";
+
+  var myButtonPosition = realButton.offsetWidth + 20;
+
+  var button = document.createElement('a');
+  button.setAttribute('class', 'btn_green_white_innerfade btn_border_2px btn_medium');
+  button.setAttribute('href', 'http://steamworkshop.download/download/view/' + id);
+  button.setAttribute('style', 'right: ' + myButtonPosition + 'px;');
+
+  button.innerHTML = '<div class="subscribeIcon"></div>' +
+    '<span class="subscribeText">' +
+    '<div class="subscribeOption subscribe selected" id="SubscribeItemOptionAdd">Download</div>' +
+    '</span>';
+
+  // append the element after the real subscribe button
+  if (realButton.nextSibling) {
+    realButton.parentNode.insertBefore(button, realButton.nextSibling);
+  }
+  else {
+    realButton.parentNode.appendChild(button);
+  }
+}
